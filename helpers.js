0da1a6cf7b6340462a7b630fadce74fe14f55004
Andrew <vozniukandrey@gmail.com>
2020-06-08 12:01:27 +0300

    add files_txt to gitignore

--- helpers.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const getDescription = () => {
-  let description = ''
-  const lines = fs.readFileSync('description.txt').toString().split('\n')
-  lines.forEach((el) => { description += el })
-  return description
-}
-module.exports = getDescription
